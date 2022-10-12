552d9f2e094cc2ef5765bc66a07348a6b6d3bc1b
vishwa <vishwadeep.ghimire@gmail.com>
2022-10-12 13:36:37 -0500

    Shipping service implementation

--- shipping-service/src/main/java/com/example/shippingservice/entities/Shipping.java
+++ shipping-service/src/main/java/com/example/shippingservice/entities/Shipping.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -17,7 +19,10 @@ import javax.persistence.Id;
 @Setter
 public class Shipping {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long shippingId;
+    private Long userId;
+    private Long orderId;
     private String city;
     private String state;
     private String stAddress;
--- shipping-service/src/main/java/com/example/shippingservice/services/impl/ShippingServiceImpl.java
+++ shipping-service/src/main/java/com/example/shippingservice/services/impl/ShippingServiceImpl.java
@@ -1,4 +1,33 @@
 package com.example.shippingservice.services.impl;
 
-public class ShippingServiceImpl {
+import com.example.commonsmodule.DTOs.OrderDTO;
+import com.example.commonsmodule.security.CommonSecurityUtils;
+import com.example.shippingservice.entities.DTOs.ShippingDTO;
+import com.example.shippingservice.entities.Shipping;
+import com.example.shippingservice.repositories.ShippingRepository;
+import com.example.shippingservice.services.ShippingService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ShippingServiceImpl implements ShippingService {
+    @Autowired
+    ShippingRepository shippingRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public ShippingDTO shipping(OrderDTO orderDTO) {
+        Shipping shipping = modelMapper.map(orderDTO.getShippingDTO(), Shipping.class);
+        shipping.setUserId(CommonSecurityUtils.getCurrentUserId().get());
+        shipping.setStAddress(orderDTO.getShippingDTO().getStAddress());
+        shipping.setCity(orderDTO.getShippingDTO().getCity());
+        shipping.setState(orderDTO.getShippingDTO().getState());
+        shipping.setZip(orderDTO.getShippingDTO().getZip());
+        shipping.setOrderId(orderDTO.getOrderId());
+        shippingRepository.save(shipping);
+        return modelMapper.map(shipping, ShippingDTO.class);
+    }
 }
